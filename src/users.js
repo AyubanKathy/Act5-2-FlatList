85073fefd6e68e0b7cf477a5d79bb2e3b723f860
AyubanKathy <kathquileopas@gmail.com>
2023-11-17 17:33:05 +0800

    create

---
+++ src/users.js
@@ -0,0 +1,202 @@
+export const users = [
+    {
+        id: 1,
+        name: {
+            firstname: "JESSE",
+            lastname: "AMANG",
+            middlename: "A",
+        },
+        town: "Corella",
+        province: "Bohol",
+    },
+    {
+        id: 2,
+        name: {
+            firstname: "KENT IVAN",
+            lastname: "AMORA",
+            middlename: "",
+        },
+        town: "Cortes",
+        province: "Bohol",
+    },
+    {
+        id: 3,
+        name: {
+            firstname: "RIA MAE",
+            lastname: "ANGCAHAN",
+            middlename: "P",
+        },
+        town: "Catigbian",
+        province: "Bohol",
+    },
+    {
+        id: 4,
+        name: {
+            firstname: "DEBORAH JANE",
+            lastname: "ANTOPINA",
+            middlename: "",
+        },
+        town: "Tagbilaran City",
+        province: "Bohol",
+    },
+    {
+        id: 5,
+        name: {
+            firstname: "KATHRINE MAE",
+            lastname: "AYUBAN",
+            middlename: "Q",
+        },
+        town: "Dauis",
+        province: "Bohol",
+    },
+    {
+        id: 6,
+        name: {
+            firstname: "BERNADETH",
+            lastname: "BAÑAGA",
+            middlename: "A",
+        },
+        town: "Loboc",
+        province: "Bohol",
+    },
+    {
+        id: 7,
+        name: {
+            firstname: "AIREN MAE",
+            lastname: "BONGCARAS",
+            middlename: "M",
+        },
+        town: "Dauis",
+        province: "Bohol",
+    }, 
+    {
+        id: 8,
+        name: {
+            firstname: "JAMES WARREN",
+            lastname: "BUCIA",
+            middlename: "",
+        },
+        town: "Cortes",
+        province: "Bohol",
+    },
+    {
+        id: 9,
+        name: {
+            firstname: "RAYMOND",
+            lastname: "BUÑOL",
+            middlename: "T",
+        },
+        town: "Cortes",
+        province: "Bohol",
+    },
+    {
+        id: 10,
+        name: {
+            firstname: "MARLON",
+            lastname: "CARBONILLA",
+            middlename: "",
+        },
+        town: "San Isidro",
+        province: "Bohol",
+    },
+    {
+        id: 11,
+        name: {
+            firstname: "NATHANIEL LOUISE",
+            lastname: "COQUILLA",
+            middlename: "",
+        },
+        town: "Antequera",
+        province: "Bohol",
+    },
+    {
+        id: 12,
+        name: {
+            firstname: "RHIZABELLE JHAINE",
+            lastname: "DALEN",
+            middlename: "C",
+        },
+        town: "Dauis",
+        province: "Bohol",
+    },
+    {
+        id: 13,
+        name: {
+            firstname: "MARY ANN",
+            lastname: "DELA CRUZ",
+            middlename: "",
+        },
+        town: "Balilihan",
+        province: "Bohol",
+    },
+    {
+        id: 14,
+        name: {
+            firstname: "SHERLINE ROSE",
+            lastname: "DONDOYANO",
+            middlename: "",
+        },
+        town: "Balilihan",
+        province: "Bohol",
+    },
+    {
+        id: 15,
+        name: {
+            firstname: "DAISY DIANE",
+            lastname: "EUSALAN",
+            middlename: "E",
+        },
+        town: "Dauis",
+        province: "Bohol",
+    },
+    {
+        id: 16,
+        name: {
+            firstname: "NELBREY JILLIAN",
+            lastname: "ESCUADRO",
+            middlename: "",
+        },
+        town: "Sindangan",
+        province: "Zamboanga Del Norte",
+    },
+    {
+        id: 17,
+        name: {
+            firstname: "LOUI",
+            lastname: "FUENTES",
+            middlename: "",
+        },
+        town: "Corella",
+        province: "Bohol",
+    },
+    {
+        id: 18,
+        name: {
+            firstname: "RODEL",
+            lastname: "GARCIA",
+            middlename: "J",
+        },
+        town: "Balilihan",
+        province: "Bohol",
+    },
+    {
+        id: 19,
+        name: {
+            firstname: "MAYETH",
+            lastname: "GAL",
+            middlename: "",
+        },
+        town: "Maribojoc",
+        province: "Bohol",
+    },
+    {
+        id: 20,
+        name: {
+            firstname: "JOANA",
+            lastname: "JAGUNOS",
+            middlename: "C",
+        },
+        town: "Balilihan",
+        province: "Bohol",
+    },
+]
